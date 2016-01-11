8b308cf5159898db7f73729ec8752848f89e91fe
lee seon <leeseon@leeui-MacBook.local>
2016-01-11 20:00:44 +0900

    initial commit

---
+++ lesson 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson 1
+//
+//  Created by lee seon on 2016. 1. 11..
+//  Copyright © 2016년 lee seon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lesson 1Tests/lesson_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  lesson_1Tests.swift
+//  lesson 1Tests
+//
+//  Created by lee seon on 2016. 1. 11..
+//  Copyright © 2016년 lee seon. All rights reserved.
+//
+
+import XCTest
+@testable import lesson_1
+
+class lesson_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
